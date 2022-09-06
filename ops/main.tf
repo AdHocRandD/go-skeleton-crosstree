035cb1fbca8af12e8d0fba69c01de2a0d8873379
Bill Mill <bill@billmill.org>
2022-09-06 16:21:49 -0400

    remove terraform stuff

--- ops/main.tf
+++ ops/main.tf
@@ -2,28 +2,12 @@
 # * is it best practice to pin a version for all dependencies?
 
 provider "aws" {
-  access_key                  = "test"
-  secret_key                  = "test"
-  region                      = "us-east-1"
-  s3_force_path_style         = false
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-
-  endpoints {
-    s3 = "http://s3.localhost.localstack.cloud:4566"
-  }
-}
-provider "aws" {
   region = "{{ .Region }}"
 }
 
 terraform {
   required_version = ">= 1.0"
 
-  backend "local" {
-    path = "./terraform.tfstate"
-  }
   backend "s3" {
     bucket         = "{{ .TerraformBucket }}"
     key            = "terraform.tfstate"
