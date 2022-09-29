16966f32bfb0921736d377aa0a30a0e26d96d059
Bill Mill <bill@billmill.org>
2022-09-29 14:02:30 -0400

    more variables

--- ops/variables.tf
+++ ops/variables.tf
@@ -32,3 +32,35 @@ variable "region" {
   type        = string
   default     = "{{ .Region }}"
 }
+
+variable "vpc_public_subnet_cidr_blocks" {
+  description = "List of public subnet cidrs to use with the VPC, each subnet gets a unique availability zone. Amount of subnets must be under amount of availability zones available in a region."
+  type        = list(string)
+  default = [
+    "10.0.5.0/26",
+    "10.0.5.64/26",
+    "10.0.5.128/26"
+  ]
+}
+
+variable "vpc_private_subnet_cidr_blocks" {
+  description = "List of private subnet cidrs to use with the VPC, each subnet gets a unique availability zone. Amount of subnets must be under amount of availability zones available in a region."
+  type        = list(string)
+  default = [
+    "10.0.3.0/26",
+    "10.0.3.64/26",
+    "10.0.3.128/26"
+  ]
+}
+
+variable "tags" {
+  description = "A map of resource tags to append to all resources"
+  type        = map(string)
+  default = {
+    environment  = "development"
+    organization = "{{ .Organization }}"
+    project      = "{{ .ProjectName }}"
+    repository   = "{{ .RepoName }}"
+    project_id   = "{{ .ProjectID }}"
+  }
+}
