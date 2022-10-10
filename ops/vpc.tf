8a5350c87e2d09174e2c570b6c8534f92613c4cc
Bill Mill <bill@billmill.org>
2022-10-10 14:29:43 -0400

    proper route table name

--- ops/vpc.tf
+++ ops/vpc.tf
@@ -89,7 +89,7 @@ resource "aws_vpc_endpoint" "s3" {
   service_name      = "com.amazonaws.${var.region}.s3"
   vpc_endpoint_type = "Gateway"
   route_table_ids = [
-    aws_vpc.main.main_route_table
+    aws_vpc.main.main_route_table_id
   ]
 
   tags = var.tags
