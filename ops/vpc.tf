9e26f56d4032d93ba41d665a99e06ffdd71aa99d
Bill Mill <bill@billmill.org>
2022-10-10 14:24:23 -0400

    link the vpc route table to the s3 endpoint

--- ops/vpc.tf
+++ ops/vpc.tf
@@ -88,6 +88,9 @@ resource "aws_vpc_endpoint" "s3" {
   vpc_id            = aws_vpc.main.id
   service_name      = "com.amazonaws.${var.region}.s3"
   vpc_endpoint_type = "Gateway"
+  route_table_ids = [
+    aws_vpc.main.main_route_table
+  ]
 
   tags = var.tags
 }
